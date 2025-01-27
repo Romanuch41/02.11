6c98bb214f6c89ff5573142c4b5f1b2ad2299e0c
Romanuch41 <pfonit@mail.ru>
2025-01-27 23:16:38 +0800

    added task 3

---
+++ 02.11.2/02.11.2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02._11._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            Nullable<bool> result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filePath = fileDialog.FileName;
+                this.path = filePath;
+                using (StreamReader stream = new StreamReader(filePath))
+                {
+                    string content = stream.ReadToEnd();
+                    box_text.Text = content;
+                    stream.Close();
+                }
+            }
+            edit_file.IsEnabled = true;
+        }
+
+        private void edit_file_Click(object sender, RoutedEventArgs e)
+        {
+            Window_edit_file window_Edit_File = new Window_edit_file();
+            window_Edit_File.Owner = this;
+            window_Edit_File.Show();
+            window_Edit_File.edit_text.Text = box_text.Text;
+            window_Edit_File.path = this.path;
+        }
+    }
+}
---
+++ 02.11.2/02.11.2/Window_edit_file.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _02._11._2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window_edit_file.xaml
+    /// </summary>
+    public partial class Window_edit_file : Window
+    {
+        public string path = "";
+        public Window_edit_file()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_file_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void save_file_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter write = new StreamWriter(this.path, false))
+            {
+                write.Write(edit_text.Text);
+                write.Close();
+            }
+        }
+    }
+}
